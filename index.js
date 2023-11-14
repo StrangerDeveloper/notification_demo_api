15b926b7f6dc44709191be8a99a2d702f8ea31c4
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-11-14 11:19:41 +0500

    remove home page

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app  = express();
 app.use(express.json());
 
 
-app.use("/", (req, res) => {
-    console.log("Main Page Connected!");
-    });
+// app.use("/", (req, res) => {
+//     console.log("Main Page Connected!");
+//     });
 app.use("/api", require("./routes/app.routes"));
 
 
