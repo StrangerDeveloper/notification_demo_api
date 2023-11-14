29da85e38187dff01be44c466f19308b94cb6d22
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-11-14 11:11:11 +0500

    remove api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app  = express();
 
 app.use(express.json());
 
-app.use("/api", require("./routes/app.routes"));
+//app.use("/api", require("./routes/app.routes"));
 app.use("/", (req, res) => {
     console.log("Main Page Connected!");
     });
