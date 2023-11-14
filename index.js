5ea54f9f49e6ccbc9edb1eced1154b9b280db253
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-11-14 11:08:28 +0500

    main page changes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app  = express();
 app.use(express.json());
 
 app.use("/api", require("./routes/app.routes"));
+app.use('/', (req, res) => {
+    console.log("Main Page Connected!");
+    });
+
 
 app.listen(4000, function(){
     console.log("Server is connected and ready to go.");
